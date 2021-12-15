8e30771f33422e7a0360ab9837e4eb7c79013ccc
Selene Grossett <selene_816@hotmail.com>
2021-12-15 18:40:46 +0000

    Used this to grab the value of the textarea in question, and determine the length of that input value and update the counter on the page

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $('.new-tweet textarea').on('input', function() {
-   
+    const tweetLength = $(this).val().length;
+    const maxLength = 140;
+    let length = maxLength - tweetLength;
+
+    $('.counter').text(length);
   });
 });
 
