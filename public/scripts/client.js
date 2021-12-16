5f80459acf5aeb63cafb288466c083b0776f0399
Selene Grossett <selene_816@hotmail.com>
2021-12-16 07:52:32 +0000

    Hardcoded the tweets in order to create HTML markup using template literals. The function should return a tweet to the caller

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,8 +7,32 @@
 $(document).ready(function() {
 
   const createTweetElement = function(tweetObj) {
-    
-    
+    const $tweet = $(
+      `<article class="tweet">
+        <header>
+          <div class="profile">
+            <img src="${tweetObj.user.avatars}" alt="${tweetObj.user.name}-profile-pic" class="profile-pic">
+            <h4>${tweetObj.user.name}</h4>
+          </div>
+          
+          <div class="handle">
+            <h5>${tweetObj.user.handle}</h5>
+          </div>
+        </header>
+        <div>
+          <p>${tweetObj.content.text}</p>
+          <hr>
+        </div>
+        <footer>
+          <div><b>${tweetObj.created_at}</b></div>
+          <div>
+            <button class="icon flag"><i class="fas fa-flag"></i></button>
+            <button class="icon retweet"><i class="fas fa-retweet"></i></button>
+            <button class="icon heart"><i class="fas fa-heart"></i></button>
+          </div>
+        </footer>
+      </article>`);
+    return $tweet;
   }
 
 });
